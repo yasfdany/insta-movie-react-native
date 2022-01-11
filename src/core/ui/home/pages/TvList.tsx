399448f847aa48246895a7ff031d529730eee21f
Ya'suf Dany <yasfdany@gmail.com>
2022-01-11 23:06:57 +0700

    Search input

--- src/core/ui/home/pages/TvList.tsx
+++ src/core/ui/home/pages/TvList.tsx
@@ -1,21 +1,61 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  TextInput,
 } from 'react-native';
+import Colors from '../../../constants/Colors';
 import GS from '../../../constants/GlobalStyles';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { TouchableRipple } from 'react-native-paper';
 
 const TvList = () => {
+    const [text, onSearchChange] = useState("")
+
+    useEffect(() => {
+    }, [text])
+
     return (
-        <View style={GS.container}>
-            <Text>Tv Screen</Text>
+        <View style={[GS.flex, GS.column]}>
+            <View style={[GS.row, GS.crossCenter, styles.containerInput]}>
+                <Icon style={{marginHorizontal: 12}} name="search" size={wp(6)} />
+                <TextInput 
+                    value={text}
+                    onChangeText={onSearchChange}
+                    placeholder="Search here..."
+                    style={{flex: 1}}
+                />
+                { text.length > 0 ? 
+                    <TouchableRipple
+                        borderless
+                        onPress={() => {
+                            onSearchChange("")
+                        }}
+                        style={{marginHorizontal: 12}}
+                        rippleColor="rgba(0, 0, 0, .32)">
+                        <Icon name="close" size={wp(6)} /> 
+                    </TouchableRipple>
+                : null }
+            </View>
+            <View style={{
+                backgroundColor: 'rgba(0, 0, 0, .1)',
+                width: '100%',
+                height: 1,
+                marginTop: 6,
+            }}/>
         </View> 
     )
 }
+
+const styles = StyleSheet.create({
+  containerInput: {
+    margin: 14,
+    backgroundColor: 'rgba(0,0,0,0.1)',
+    borderRadius: 8,
+  },
+});
 
 export default TvList;
